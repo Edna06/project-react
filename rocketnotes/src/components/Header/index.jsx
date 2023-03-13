04f9bc876a68e091591031d72261b1aa78274bf1
Edna <edna.maria887@gmail.com>
2023-03-12 21:13:46 -0300

    addition image and name in profile

--- rocketnotes/src/components/Header/index.jsx
+++ rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,17 @@
-import { Container } from './style'
+import { Container, Profile } from './style'
 
 export function Header(){
   return(
     <Container>
-      
+      <Profile>
+        <img src="https://github.com/Edna06.png" 
+        alt="foto do usuário" />
+
+      <div>
+        <span>Bem-vindo,</span>
+        <strong>Edna Maria</strong>
+      </div>
+      </Profile>
     </Container>
   )
 }
--- rocketnotes/src/components/Header/style.js
+++ rocketnotes/src/components/Header/style.js
@@ -1,20 +1,46 @@
 import styled from 'styled-components'
 
 export const Container = styled.header`
-/* para que o cabeçalho fique fixo no header */
-grid-area: header;
+  grid-area: header;
 
-height: 105px;
-width: 100%;
+  height: 105px;
+  width: 100%;
 
-border-bottom-width: 1px;
-border-bottom-style: solid;
-border-bottom-color: ${({theme}) => theme.COLORS.BACKGROUND_700};
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
 
-display: flex;
-justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 
-padding: 0 80px;
+  padding: 0 80px;
+`;
 
-background: red;
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+
+
+  > img {
+    width: 56px;
+    height: 56px;
+    border-radius: 50%;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    margin-left: 16px;
+    line-height: 24px;
+
+    span {
+      font: 14px;
+      color: ${({ theme }) => theme.COLORS.GRAY_100};
+    }
+
+    strong {
+      font: 18px;
+      color: ${({ theme }) => theme.COLORS.WHITE};
+    }
+  }
 `
