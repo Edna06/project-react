e5188af524eec3c5555db151bc9f39d1f3071f24
Edna <edna.maria887@gmail.com>
2023-04-03 00:39:22 -0300

    added registering user

--- rocketnotes/src/pages/SignUp/index.jsx
+++ rocketnotes/src/pages/SignUp/index.jsx
@@ -1,12 +1,45 @@
+
+import { useState } from "react" 
 import { FiUser ,FiMail, FiLock } from "react-icons/fi"
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+
+//enviando informações para o backend 
+import {api} from "../../services/api"
 
 import {Input} from "../../components/Input/index"
 import {Button} from "../../components/Button/index"
 
 import { Container, Form, Background } from "./styles"
 
+
 export function SignUp(){
+  const [name, setName] = useState(""); 
+  const [email, setEmail] = useState(""); 
+  const [password, setPassword] = useState(""); 
+  
+  const navigate = useNavigate()
+  
+   function handleSingleUp() {
+    if(!name || !email || !password) {
+      return alert("Preencha todos os campos!")
+    }
+  
+      
+      api.post("/users", {name, email, password}) 
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!")
+        navigate("/"); 
+      }) 
+      .catch(error =>{
+        if(error.response){
+           
+          alert(error.response.data.message)
+        } else {
+          alert("Não foi possível cadastrar.")
+        }
+      } ) 
+  }    
+
   return(
     <Container>
       <Background/>
@@ -23,21 +56,23 @@ export function SignUp(){
        placeholder="Nome"
        type="text"
        icon={FiUser}
-       />
+       onChange = {event => setName( event.target.value)}/>
        
        <Input
        placeholder="E-mail"
        type="text"
        icon={FiMail}
+       onChange = {event => setEmail( event.target.value)}
        />
 
       <Input
        placeholder="Senha"
        type="password" 
        icon={FiLock}
+       onChange = {event => setPassword( event.target.value)}
        />
 
-        <Button title="Cadastrar"/>
+        <Button title="Cadastrar" onClick = {handleSingleUp}/>
 
         <Link to="/">
         Voltar para o login
