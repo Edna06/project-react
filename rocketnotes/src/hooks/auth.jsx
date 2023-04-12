6be715d15b7f5e5414e2579c95772f27ae9b415c
Edna <edna.maria887@gmail.com>
2023-04-12 17:44:12 -0300

    use local storage

--- rocketnotes/src/hooks/auth.jsx
+++ rocketnotes/src/hooks/auth.jsx
@@ -1,5 +1,5 @@
 //arquivo de contexto de autenticação
-import { createContext, useContext, useState} from 'react' 
+import { createContext, useContext, useState, useEffect} from 'react' 
 
 import {api} from "../services/api" //para que eu consiga conectar com a api
 
@@ -15,11 +15,14 @@ function AuthProvider({ children }) {
   async function signIn({email, password}){ 
     try { 
        const response = await api.post("/sessions", {email, password})
-
        const {user, token} = response.data; 
 
-      api.defaults.headers.authorization = `Bearer ${token}`  
 
+       
+       localStorage.setItem('@rocketnotes:user', JSON.stringify(user)) 
+       localStorage.setItem('@rocketnotes:token', token)
+
+      api.defaults.headers.authorization = `Bearer ${token}`  
       setData({user, token})
 
       } catch(error){ 
@@ -31,6 +34,22 @@ function AuthProvider({ children }) {
         }
     }
   }
+
+
+  useEffect(() => {
+   const user = localStorage.getItem('@rocketnotes:user')
+   const token = localStorage.getItem('@rocketnotes:token') 
+
+   if( token && user) {
+    api.defaults.headers.authorization = `Bearer ${token}`
+
+    setData({
+      token,
+      user: JSON.parse(user)
+    })
+   }
+
+  }, [])
 
   return (
     <AuthContext.Provider
